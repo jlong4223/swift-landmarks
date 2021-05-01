241e95fd4c6f46bde8c9250a5af92a21b18e7425
Jared Long <jlong4223@gmail.com>
2021-05-01 17:36:14 -0500

    separates files into groups based on type - model, resources, views ; & adds model for grabbing landmark data for landmark array

---
+++ Landmarks/Model/ModelData.swift
@@ -0,0 +1,34 @@
+//
+//  ModelData.swift
+//  Landmarks
+//
+//  Created by Jared  Long on 5/1/21.
+//
+
+import Foundation
+
+// creating an array of landmarks by loading the json file
+var landmarks: [Landmark] = load("landmarkData.json")
+
+// creating the load function that will handle the file passed into it, which is defined above
+func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+    else {
+        fatalError("Couldn't find \(filename) in main bundle.")
+    }
+
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+    }
+
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+    }
+}
