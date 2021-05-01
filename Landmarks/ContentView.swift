eaf5f578621481ccbb154baf8a237f402d6ecc82
Jared Long <jlong4223@gmail.com>
2021-04-30 19:29:00 -0500

    First commit and changes name

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Hello, Jared!")
+            .font(.title)
+            .fontWeight(.bold)
             .padding()
     }
 }
