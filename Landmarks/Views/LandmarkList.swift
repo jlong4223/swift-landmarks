5c12315e9bbf4e48681948c712baa731ef63f224
Jared Long <jlong4223@gmail.com>
2021-05-03 13:19:36 -0500

    adds navigation link to render the details for each landmark

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -10,9 +10,16 @@ import SwiftUI
 //this view is for creating the list of the already created rows
 struct LandmarkList: View {
     var body: some View {
+        
+        NavigationView{
 //        dynamically creating a list of landmarks from the array and creating a row for each - basically looping the array
-        List(landmarksArray){ landmark in
-            LandmarkRow(landmark: landmark)
+            List(landmarksArray){ landmark in
+//                Creating a nav link for each landmark that renders the details 
+                NavigationLink(destination: LandmarkDetail()){
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
         }
     }
 }
