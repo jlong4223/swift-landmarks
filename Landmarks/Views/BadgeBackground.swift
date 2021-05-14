7b8009c559173592044302194b44007e90855370
Jared Long <jlong4223@gmail.com>
2021-05-13 20:21:39 -0500

    adds QuadCurve and GeometryReader to enhance the shapes curves and size

--- Landmarks/Views/BadgeBackground.swift
+++ Landmarks/Views/BadgeBackground.swift
@@ -9,15 +9,17 @@ import SwiftUI
 
 struct BadgeBackground: View {
     var body: some View {
+//        using geometryReader to make the shae the same size as the view
+        GeometryReader { geometry in
 //        paths are used to combine lines, curves and drawings
         Path { path in
-            var width: CGFloat = 100.0
+            var width: CGFloat = min(geometry.size.width, geometry.size.height)
             let height = width
 //            move(to:) moves the drawing cursor within the bounds of a shape
             path.move(
                 to: CGPoint(
                     x: width * 0.95,
-                    y: height * 0.20
+                    y: height * ( 0.20 + HexagonParameters.adjustment)
                 )
             )
             HexagonParameters.segments.forEach{ segment in
@@ -27,9 +29,21 @@ struct BadgeBackground: View {
                         y: height * segment.line.y
                     )
                 )
+//                using addQuadCurve to draw out better curves
+                path.addQuadCurve(
+                    to: CGPoint(
+                        x: width * segment.curve.x,
+                        y: height * segment.curve.y
+                    ),
+                    control: CGPoint(
+                        x: width * segment.control.x,
+                        y: height * segment.control.y
+                    )
+                )
             }
         }
         .fill(Color.black)
+        }
     }
 }
 
