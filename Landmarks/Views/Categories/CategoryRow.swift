8527dfcb6a1a4cb3572d5c6480ff6bf4d897cd10
Jared Long <jlong4223@gmail.com>
2021-05-20 22:49:06 -0500

    turns each category item into a navigation link

--- Landmarks/Views/Categories/CategoryRow.swift
+++ Landmarks/Views/Categories/CategoryRow.swift
@@ -23,7 +23,10 @@ struct CategoryRow: View {
                 HStack(alignment: .top, spacing: 0){
 //                only looping through first 3 which comes from the prefix defined below
                     ForEach(items){landmark in
-                        CategoryItem(landmark: landmark)
+//                        makes each landmark a nav link
+                        NavigationLink(destination: LandmarkDetail(landmark: landmark)){
+                            CategoryItem(landmark: landmark)
+                        }
                     }
                 }
             }
