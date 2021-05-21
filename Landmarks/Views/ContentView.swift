d5eb26e7324b6933f3419e0b6fbdc02b040eff6a
Jared Long <jlong4223@gmail.com>
2021-05-20 22:50:21 -0500

    creates tabs and tab views to show the featured as one and the list as another

--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -8,8 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selection: Tab = .featured
+
+//    creating two tabs
+    enum Tab {
+            case featured
+            case list
+        }
+    
     var body: some View {
-        LandmarkList()
+        TabView(selection: $selection) {
+                   CategoryHome()
+//                    creating a tab label - the label is connected to this view so when clicked it will render this one
+                    .tabItem {
+                        Label("Featured", systemImage: "star")
+                    }
+                    .tag(Tab.featured)
+
+                   LandmarkList()
+                    .tabItem {
+                        Label("List", systemImage: "list.bullet")
+                    }
+                    .tag(Tab.list)
+               }
     }
 }
 
