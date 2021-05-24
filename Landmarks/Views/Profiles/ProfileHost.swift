fdfe4e173b9517fdee9213028a5ba72fd1d7868e
Jared Long <jlong4223@gmail.com>
2021-05-23 22:13:23 -0500

    created an edit button with a static text view before creating edit view

--- Landmarks/Views/Profiles/ProfileHost.swift
+++ Landmarks/Views/Profiles/ProfileHost.swift
@@ -8,19 +8,30 @@
 import SwiftUI
 
 struct ProfileHost: View {
+    @Environment(\.editMode) var editMode
+    @EnvironmentObject var modellData: ModelData
     @State private var draftProfile = Profile.default
     
     var body: some View {
         VStack(alignment: .leading, spacing: 20) {
-//            passing the defined state var as a param to the profilesSummary
-                ProfilesSummary(profile: draftProfile)
+            HStack{
+                Spacer()
+                EditButton()
             }
-            .padding()
+            
+            if editMode?.wrappedValue == .inactive {
+                            ProfilesSummary(profile: modellData.profile)
+                        } else {
+                            Text("Profile Editor")
+                        }
+                    }
+                    .padding()
     }
 }
 
 struct ProfileHost_Previews: PreviewProvider {
     static var previews: some View {
         ProfileHost()
+            .environmentObject(ModelData())
     }
 }
