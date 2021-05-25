2312be54719643626cd7d83c6d72881e0b0866b9
Jared Long <jlong4223@gmail.com>
2021-05-24 22:09:11 -0500

    begins the pageViewController and functionality

---
+++ Landmarks/Views/PageView/PageViewController.swift
@@ -0,0 +1,27 @@
+//
+//  PageViewController.swift
+//  Landmarks
+//
+//  Created by Jared  Long on 5/24/21.
+//
+
+import Foundation
+import UIKit
+
+struct PageViewController<Page: View>: UIViewControllerRepresentable {
+//    stores an array of Page instances
+    var pages: [Page]
+
+    func makeUIViewController(context: Context) -> UIPageViewController {
+        let pageViewController = UIPageViewController(
+            transitionStyle: .scroll,
+            navigationOrientation: .horizontal
+            )
+        return pageViewController
+    }
+    
+    func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
+            pageViewController.setViewControllers(
+                [UIHostingController(rootView: pages[0])], direction: .forward, animated: true)
+        }
+}
