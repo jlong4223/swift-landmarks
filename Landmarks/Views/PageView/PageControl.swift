af78026cc282e70b353928bad838ecd612ee8075
Jared Long <jlong4223@gmail.com>
2021-06-03 22:26:12 -0500

    creates new control  to conform to the UIViewRepresentable protocol

---
+++ Landmarks/Views/PageView/PageControl.swift
@@ -0,0 +1,26 @@
+//
+//  PageControl.swift
+//  Landmarks
+//
+//  Created by Jared  Long on 6/3/21.
+//
+
+import SwiftUI
+
+import UIKit
+
+struct PageControl: UIViewRepresentable {
+    var numberOfPages: Int
+    @Binding var currentPage: Int
+
+    func makeUIView(context: Context) -> UIPageControl {
+        let control = UIPageControl()
+        control.numberOfPages = numberOfPages
+
+        return control
+    }
+
+    func updateUIView(_ uiView: UIPageControl, context: Context) {
+        uiView.currentPage = currentPage
+    }
+}
